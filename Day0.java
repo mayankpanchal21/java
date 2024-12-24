15af9a7abd7dd4a473b08b7537f240d971ef03dd
mayankpan25122024 <indusmk90@gmail.com>
2024-12-25 02:17:52 +0530

    Intro

--- Day0.java
+++ Day0.java
@@ -1,7 +1,8 @@
+// Fundamentals Of Java Programming language
 public class Day0 {
   public static void main(String[] args) {
-    byte a = 127 , b = 5 , c = 6;
-    System.out.println(a + b + c);
+    String name = "mayank";
+    System.out.println(name +" ,How are you?");
   }
 }
 
